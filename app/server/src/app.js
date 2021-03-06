a11dc88bf8a2db74c148dc3d6291fa3a780ffef5
Eugene Sinamban <sinamban_e@cyberowl.co.jp>
2021-03-06 16:46:17 +0900

    add additional routes

--- app/server/src/app.js
+++ app/server/src/app.js
@@ -1,9 +1,10 @@
 const express = require('express')
-
+const bodyParser = require('body-parser')
 const postRoutes = require('./routes/posts')
 
 const app = express()
 
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(postRoutes)
 
 app.listen(8090, () => {
--- app/server/src/routes/posts.js
+++ app/server/src/routes/posts.js
@@ -3,5 +3,8 @@ const router = express.Router()
 const controller = require('../controller/postController')
 
 router.get('/', controller.postIndex)
+router.get('/posts/:id', controller.postShow)
+router.post('/posts', controller.postInsert)
+router.patch('/posts/:id', controller.postUpdate)
 
 module.exports = router
