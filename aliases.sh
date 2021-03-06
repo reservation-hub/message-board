6f9830f5d9826fdb535475d182c9d4e1a84c76bb
Eugene Sinamban <sinamban_e@cyberowl.co.jp>
2021-03-06 15:00:26 +0900

    set up basic infrastructure

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,4 @@
-alias board-build="docker-compose build server"
-alias board-lcl="board-build && docker-compose up server"
-alias board-lcl-bash="docker-compose exec server bash"
+alias board-build="docker-compose build server client"
+alias board-lcl="board-build && docker-compose up server client"
+alias board-lcl-server-bash="docker-compose exec server bash"
+alias board-lcl-client-bash="docker-compose exec client bash"
