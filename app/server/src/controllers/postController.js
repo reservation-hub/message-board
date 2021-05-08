758823f0ffe1b8f63de1c179e1bbfffe1ca263d6
sabi1125 <sabir_barahi4@hotmail.com>
2021-05-09 04:10:33 +0900

    error handlers

--- app/server/src/controllers/postController.js
+++ app/server/src/controllers/postController.js
@@ -2,45 +2,49 @@ const Post = require('../models/post')
 const { errorHandler } = require('./crudController')
 const { filterUndefined } = require('../../lib/filter')
 
-exports.postIndex = (req, res) => {
-    
+exports.postIndex = (req, res,next) => {
+
     Post.find({}).exec()
     .then(posts => {
         if (!posts.length) {
-            return res.status(404).send({message: 'No post found!'})
+            next({error:{name:"CastError"}})
         }
         res.send(posts)
     })
-    .catch(e => errorHandler(e, res))
+    .catch(next)
 }
 
-exports.postInsert = (req, res) => {
+exports.postInsert = (req, res,next) => {
+
     const { title, name, message, password } = req.body
+
     const post = new Post({title, name, message, password})
     post.save()
     .then(result => res.status(201).send(result))
-    .catch(e => errorHandler(e, res))
+    .catch(next)
     
 }
 
-exports.postUpdate = async (req, res) => {
-    const { title, name, message,password} = req.body
+exports.postUpdate = async (req, res,next) => {
+    const { title, name, message, password} = req.body
     const { id } = req.params
     const whiteList = filterUndefined({ title, name, message, password })
     Post.findByIdAndUpdate(id, whiteList, {new: true}).exec()
     .then(post => {
-        if (!post) return res.status(404).send({message: 'No post matched'})
+        if (!post){
+            next({error:{name:"CastError"}})
+        }
         return res.send(post)
     })
-    .catch(e => errorHandler(e, res))
+    .catch(next)
 }
 
-exports.postDelete = (req, res) => {
+exports.postDelete = (req, res,next) => {
     const { id:_id } = req.params
     Post.findOneAndDelete({_id}).exec()
     .then(result => {
         if (!result) return res.status(404).send({message: 'No post matched'})
         return res.send({message: 'Successfully deleted'})
     })
-    .catch(e => errorHandler(e, res))
+    .catch(next)
 }
