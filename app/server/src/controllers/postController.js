e9b06f878e3610946877856aded90ec2306b82e4
sabi1125 <sabir_barahi4@hotmail.com>
2021-05-10 09:46:07 +0900

    error handler

--- app/server/src/controllers/postController.js
+++ app/server/src/controllers/postController.js
@@ -1,46 +1,58 @@
 const Post = require('../models/post')
-const { errorHandler } = require('./crudController')
+const asyncHandler = require("../lib/asyncHandler")
 const { filterUndefined } = require('../../lib/filter')
+const ErrorResponse = require("../utils/errorResponse")
 
-exports.postIndex = (req, res) => {
+
+exports.postIndex = asyncHandler ((req, res,next) => {
+
     Post.find({}).exec()
     .then(posts => {
         if (!posts.length) {
-            return res.status(404).send({message: 'No post found!'})
+            return next(new ErrorResponse("There are no posts avialable at the moment",404))
+        }else{
+            res.send(posts)
         }
-        res.send(posts)
-    })
-    .catch(e => errorHandler(e, res))
-}
+        
+    });
+    
+})
 
-exports.postInsert = (req, res) => {
-    console.log(req.body)
+
+exports.postInsert = asyncHandler((req, res,next) => {
+
     const { title, name, message, password } = req.body
+
     const post = new Post({title, name, message, password})
     post.save()
     .then(result => res.status(201).send(result))
-    .catch(e => errorHandler(e, res))
     
-}
+})
 
-exports.postUpdate = async (req, res) => {
-    const { title, name, message,password} = req.body
+exports.postUpdate = asyncHandler(async (req, res,next) => {
+    const { title, name, message, password} = req.body
     const { id } = req.params
     const whiteList = filterUndefined({ title, name, message, password })
     Post.findByIdAndUpdate(id, whiteList, {new: true}).exec()
     .then(post => {
-        if (!post) return res.status(404).send({message: 'No post matched'})
-        return res.send(post)
+        if (!post) {
+            return next(new ErrorResponse("The post you are trying to find does't exist",404))
+        }else{
+            return res.send(post)
+        }
+        
     })
-    .catch(e => errorHandler(e, res))
-}
+})
 
-exports.postDelete = (req, res) => {
+exports.postDelete = asyncHandler((req, res,next) => {
     const { id:_id } = req.params
     Post.findOneAndDelete({_id}).exec()
     .then(result => {
-        if (!result) return res.status(404).send({message: 'No post matched'})
-        return res.send({message: 'Successfully deleted'})
+        if (!result){
+            return next(new ErrorResponse("The post you are trying to delete does't exist",404))
+        }else{
+            return res.send({message: 'Successfully deleted'})
+        }
+        
     })
-    .catch(e => errorHandler(e, res))
-}
+})
